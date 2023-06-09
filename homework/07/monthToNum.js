161cc17ff3f9b96d7b8f86ae8a71d62215b032f3
Yangforrok <yangboyu7@gmail.com>
2023-06-09 21:27:07 +0800

    madd.js

--- homework/07/monthToNum.js
+++ homework/07/monthToNum.js
@@ -1,18 +1,18 @@
-var monthMap = {
-    'january': "1月",
-    'february': "2月",
-    'march': "3月",
-    'april': "4月",
-    'may': "5月",
-    'june': "6月",
-    'july': "7月",
-    'august': "8月",
-    'september': "9月",
-    'october': "10月",
-    'november': "11月",
-    'december': "12月"
+const monthMap = {
+    "january": "1月",
+    "february": "2月",
+    "march": "3月",
+    "april": "4月",
+    "may": "5月",
+    "june": "6月",
+    "july": "7月",
+    "august": "8月",
+    "september": "9月",
+    "october": "10月",
+    "november": "11月",
+    "december": "12月"
   }
 function monthToNum(month) {
-    return monthMap(month)
+    return monthMap[month]
 }
-console.monthToNum(june);
+console.log(monthToNum("june"));
